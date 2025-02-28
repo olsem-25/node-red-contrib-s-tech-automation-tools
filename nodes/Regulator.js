3ffcc15e97e676440ad77b382a0af0d316703a1a
olsem-25 <64503069+olsem-25@users.noreply.github.com>
2025-02-28 15:24:32 +0000

    Fix bugs

--- nodes/Regulator.js
+++ nodes/Regulator.js
@@ -2,11 +2,18 @@
 
 module.exports = function(RED) {
     function STRegulator(config) {
-
+        const node = this;
 		RED.nodes.createNode(this, config); 
         const server = RED.nodes.getNode(config.wbserver);
+        server.setMaxListeners(cloud.getMaxListeners() + 1); // увеличиваем лимит для event
 
+        server.on("online",()=>{
+			node.status({fill:"green",shape:"dot",text:"online"});
+		});
 
+		server.on("offline",()=>{   
+			node.status({fill:"red",shape:"dot",text:"offline"});
+		});
 
     }
     RED.nodes.registerType("Regulator", STRegulator);

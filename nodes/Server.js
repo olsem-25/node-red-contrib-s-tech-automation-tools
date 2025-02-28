6285ea585062b6d82eae6557e9ca22f92abcf28f
olsem-25 <64503069+olsem-25@users.noreply.github.com>
2025-02-28 15:46:43 +0000

    Added connect to server options

--- nodes/Server.js
+++ nodes/Server.js
@@ -2,14 +2,23 @@ var mqtt = require('mqtt');
 
 module.exports = function (RED) {    
     function WBServerConfig(config) {
-            RED.nodes.createNode(this, config);   
+        RED.nodes.createNode(this, config);   
+        var node = this;
+        const id = this.id;
+        this.name = config.name;
+   
+        function connectMQTT() {
             var node = this;
-            const id = this.id;
-            this.name = config.name;
+            var options = {
+                port: node.config.mqtt_port||1883,
+                username: node.config.mqtt_username||null,
+                password: node.config.mqtt_password||null,
+                clientId:"NodeRed-"+node.id+"-"+(Math.random() + 1).toString(36).substring(7)
+            };
+            return mqtt.connect('mqtt://' + node.config.host, options);
+        }
+        node.mqtt = connectMQTT();
     }
-    RED.nodes.registerType("WB-MQTT-Server", WBServerConfig, {
-        credentials:{
-			host: {type: "text"}
-		}
-    });
+
+    RED.nodes.registerType("WB-MQTT-Server", WBServerConfig, {});
 }

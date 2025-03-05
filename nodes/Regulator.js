6417818f51c393093fed9fc9fd5fe063403d56fe
Oleg Semenov <olsem25@mail.ru>
2025-03-05 20:46:49 +0300

    feat(Regulator): добавлено чтение параметров из файла JSON и упрощено управление состоянием

--- nodes/Regulator.js
+++ nodes/Regulator.js
@@ -1,4 +1,6 @@
 
+const fs = require('fs');
+const path = require('path');
 
 module.exports = function(RED) {
     function STRegulator(config) {
@@ -6,8 +8,7 @@ module.exports = function(RED) {
 		RED.nodes.createNode(this, config); 
         const server = RED.nodes.getNode(config.wbserver);
         const name = config.name;
-        const locale = config.locale.split('-')[0];
-            
+        const locale = config.locale.split('-')[0];            
         const driver = "S-Tech tools";
         const basetopic = "/devices/";
         const requestTopic = "/tmp/ST_items_list";
@@ -18,6 +19,8 @@ module.exports = function(RED) {
         const metastate = {};
         const metarelay = {};
 
+        const ParametrsNames = JSON.parse(fs.readFileSync(path.join(__dirname, 'config', 'parameters.json'), 'utf8'));
+
         const upto = {};
         upto.en = " up to ";
         upto.ru = " до ";
@@ -26,72 +29,8 @@ module.exports = function(RED) {
         to.en = " to ";
         to.ru = " до ";
 
-        var InitComplete = false;
-
-        const ParametrsNames = {};
-        ParametrsNames.Temperature = {};
-        ParametrsNames.Temperature.target = {}; 
-        ParametrsNames.Temperature.target.en = "Target Temperature";
-        ParametrsNames.Temperature.target.ru = "Целевая температура";
-        ParametrsNames.Temperature.current = {};
-        ParametrsNames.Temperature.current.en = "Current Temperature";
-        ParametrsNames.Temperature.current.ru = "Текущая температура";
-        ParametrsNames.Temperature.current.type = "temperature";
-        ParametrsNames.Temperature.state = {};  
-        ParametrsNames.Temperature.state.en = "State"; 
-        ParametrsNames.Temperature.state.ru = "Состояние";
-        ParametrsNames.Temperature.states = {};
-        ParametrsNames.Temperature.states.off = {};
-        ParametrsNames.Temperature.states.off.en = "Off";
-        ParametrsNames.Temperature.states.off.ru = "Выкл.";
-        ParametrsNames.Temperature.states.head = {};
-        ParametrsNames.Temperature.states.head.en = "Heating";
-        ParametrsNames.Temperature.states.head.ru = "Нагрев";
-        ParametrsNames.Temperature.states.cool = {};
-        ParametrsNames.Temperature.states.cool.en = "Cooling";
-        ParametrsNames.Temperature.states.cool.ru = "Охлаждение";
-        ParametrsNames.Temperature.states.auto = {};
-        ParametrsNames.Temperature.states.auto.en = "Auto";
-        ParametrsNames.Temperature.states.auto.ru = "Авто";
-        ParametrsNames.Temperature.states.idle = {};
-        ParametrsNames.Temperature.states.idle.en = "Idle";
-        ParametrsNames.Temperature.states.idle.ru = "Ожидание";
-        ParametrsNames.Temperature.relay = {};
-        ParametrsNames.Temperature.relay.en = "Relay";
-        ParametrsNames.Temperature.relay.ru = "Реле";
-
-        ParametrsNames.Humidity = {};
-        ParametrsNames.Humidity.target = {}; 
-        ParametrsNames.Humidity.target.en = "Target Humidity";
-        ParametrsNames.Humidity.target.ru = "Целевая влажность";
-        ParametrsNames.Humidity.current = {};
-        ParametrsNames.Humidity.current.en = "Current Humidity";
-        ParametrsNames.Humidity.current.ru = "Текущая влажность";
-        ParametrsNames.Humidity.current.type = "humidity";
-        ParametrsNames.Humidity.state = {};  
-        ParametrsNames.Humidity.state.en = "State"; 
-        ParametrsNames.Humidity.state.ru = "Состояние";
-        ParametrsNames.Humidity.states = {};
-        ParametrsNames.Humidity.states.off = {};
-        ParametrsNames.Humidity.states.off.en = "Off";
-        ParametrsNames.Humidity.states.off.ru = "Выкл.";
-        ParametrsNames.Humidity.states.humidification = {};
-        ParametrsNames.Humidity.states.humidification.en = "Humidification";
-        ParametrsNames.Humidity.states.humidification.ru = "Увлажнение";
-        ParametrsNames.Humidity.states.drying = {};
-        ParametrsNames.Humidity.states.drying.en = "Drying";
-        ParametrsNames.Humidity.states.drying.ru = "Осушение";
-        ParametrsNames.Humidity.states.auto = {};
-        ParametrsNames.Humidity.states.auto.en = "Auto";
-        ParametrsNames.Humidity.states.auto.ru = "Авто";
-        ParametrsNames.Humidity.states.idle = {};
-        ParametrsNames.Humidity.states.idle.en = "Idle";
-        ParametrsNames.Humidity.states.idle.ru = "Ожидание";
-        ParametrsNames.Humidity.relay = {};
-        ParametrsNames.Humidity.relay.en = "Relay";
-        ParametrsNames.Humidity.relay.ru = "Реле";
-
-
+        var InitComplete = false;  
+        
         var device = {};
         device.enable = null;  
         device.target = null; 
@@ -116,37 +55,27 @@ module.exports = function(RED) {
                 default:
                     param = "Temperature";
             }
-            metaenable.title = {};
-            metaenable.title.en = "Enable";
-            metaenable.title.ru = "Вкл.";
+            metaenable.title = {"en":"Enable","ru":"Вкл."};
             metaenable.readonly = false;
             metaenable.type = "switch";
             metaenable.order = 1;
-            metastate.title = {};
-            metastate.title.en = ParametrsNames[param].state.en;
-            metastate.title.ru = ParametrsNames[param].state.ru;
+            metastate.title = {"en":ParametrsNames[param].state.en, "ru":ParametrsNames[param].state.ru};
             metastate.readonly = true;
             metastate.type = "text";
             metastate.order = 2;
-            metacurrent.title = {};
+            metacurrent.title = {"en":ParametrsNames[param].current.en, "ru":ParametrsNames[param].current.ru};
             metacurrent.readonly = true;
             metacurrent.order = 3;
-            metatarget.title = {};
+            metatarget.title = {"en":ParametrsNames[param].target.en, "ru":ParametrsNames[param].target.ru};
             metatarget.min = config.min;
             metatarget.max = config.max;
             metatarget.readonly = false;
             metatarget.type = "range";
             metatarget.order = 4;           
-            metarelay.title = {};
-            metarelay.title.en = ParametrsNames[param].relay.en;   
-            metarelay.title.ru = ParametrsNames[param].relay.ru;
+            metarelay.title = {"en":ParametrsNames[param].relay.en, "ru":ParametrsNames[param].relay.ru};
             metarelay.readonly = true;
             metarelay.type = "switch";
             metarelay.order = 5;
-            metatarget.title.en = ParametrsNames[param].target.en;
-            metatarget.title.ru = ParametrsNames[param].target.ru;
-            metacurrent.title.en = ParametrsNames[param].current.en;
-            metacurrent.title.ru = ParametrsNames[param].current.ru;
             metacurrent.type = ParametrsNames[param].current.type;                    
         }
         
@@ -163,7 +92,7 @@ module.exports = function(RED) {
             return new Promise((resolve, reject) => {
                 var count = 0;
                 const timer = setTimeout(() => {
-                    reject(new Error('Timeout waiting for start message'));
+                    reject(new Error('Таймаут ожидания стартового сообщения'));
                 }, timeout);
 
                 function onMessage(receivedTopic, message) {
@@ -186,10 +115,9 @@ module.exports = function(RED) {
                 node.error(`Ошибка: ${error.message}`);
                 device.target = config.target || config.min;
                 device.enable = 0;                
-                node.log('Сообщение не поступило в течение 500 мс');
+                node.log('Стартовое собщение не получено в течение 500 мс');
             }
         }
-
 
         function WriteInitialValuesToMQTT() {
             if (!(locale == "ru")) locale = "en";
@@ -237,13 +165,13 @@ module.exports = function(RED) {
         this.on('input', (msg, send, done)=>{
             if (InitComplete == false) return;
             if (typeof msg.payload != 'number'){   // Проверяем входную переменную ( number ли она )
-			 	node.error("Wrong type! msg.payload must be number.");
+			 	node.error("Неверный тип! msg.payload должен быть числом.");
 			 	if (done) {done();}
 			 	return;
 			};
 
 			if (msg.payload == device.current){     // Может значение уже установлено?        
-			 	node.debug("Value not changed. Cancel update");
+			 	node.debug("Значение не изменилось. Отмена обновления.");
 			 	if (done) {done();}
 			 	return;
 			};

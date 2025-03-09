3645e1f4282e3c99911206623de3362d765d7991
Oleg Semenov <olsem25@mail.ru>
2025-03-09 22:13:11 +0300

    fix(Curtain): исправлено название узла с "Gurtain" на "Curtain" в HTML и JS файлах

--- nodes/Curtain.js
+++ nodes/Curtain.js
@@ -1,11 +1,10 @@
 module.exports = function(RED) {
     function STCurtain(config) {
         const node = this;
-		RED.nodes.createNode(this, config); 
+        RED.nodes.createNode(this, config); 
         const server = RED.nodes.getNode(config.wbserver);
         const name = config.name;
-
-
+        
     }
     RED.nodes.registerType("Curtain", STCurtain);
 }

335a15fc4c2dc4621e532c0c6518310406db7591
Oleg Semenov <olsem25@mail.ru>
2025-03-09 23:07:28 +0300

    refactor(Curtain): удалены ненужные поля и упрощена логика отправки данных

--- nodes/Curtain.js
+++ nodes/Curtain.js
@@ -2,24 +2,21 @@ module.exports = function(RED) {
     function STCurtain(config) {
         const node = this;
         RED.nodes.createNode(this, config); 
-        const server = RED.nodes.getNode(config.wbserver);
-        const name = config.name;
 
+        node.send([{ payload: 0 }, { payload: 0 }, { payload: 0 }]);
         
         function delay(ms) {
             return new Promise(resolve => setTimeout(resolve, ms));
         }
 
-        this.on('input', (msg, send, done)=>{
+        this.on('input', (msg, _send, done)=>{
             const command = msg.payload;
             if (command == "open") {
-                node.send({ payload: 1 }, null, { payload: 0 });
+                node.send([{ payload: 1 }, null, { payload: 0 }]);
             } else if (command == "close") {
-                node.send([null, { payload: 1 }], { payload: 0 });
-                //server.sendCommand("curtain", "close", name);
+                node.send([null, { payload: 1 }, { payload: 0 }]);
             } else if (command == "stop") {
-                node.send([ { payload: 1 }, { payload: 1 }, { payload: 0 }]);
-                //server.sendCommand("curtain", "pause", name);
+                node.send([{ payload: 1 }, { payload: 1 }, { payload: 0 }]);
             } else {
                 node.error("Unknown command: " + command);
             }
@@ -29,7 +26,7 @@ module.exports = function(RED) {
 
         async function SetOuts () {
             await delay(1000); 
-            node.send({ payload: 0 }, { payload: 0 }, null);
+            node.send([{ payload: 0 }, { payload: 0 }, { payload: 0 }]);
         }
 
         

147123115c3737775be1268e868a118fcd72204a
olsem-25 <olsem25@mail.ru>
2025-06-25 15:13:21 +0300

    исправлена функция инверсии реле

--- nodes/Regulator.js
+++ nodes/Regulator.js
@@ -3,14 +3,14 @@
 module.exports = function(RED) {
     function STRegulator(config) {
         const node = this;
-		RED.nodes.createNode(this, config); 
+		RED.nodes.createNode(this, config);
         const server = RED.nodes.getNode(config.wbserver);
         const name = config.name;
-        const locale = config.locale ? config.locale.split('-')[0] : 'en'; 
+        const locale = config.locale ? config.locale.split('-')[0] : 'en';
         const driver = "S-Tech tools";
         const basetopic = "/devices/";
         const requestTopic = "/tmp/ST_items_list";
-        const responseTopic = basetopic + name; 
+        const responseTopic = basetopic + name;
         const metaenable = {};
         const metatarget = {};
         const metacurrent = {};
@@ -27,16 +27,16 @@ module.exports = function(RED) {
         to.en = " to ";
         to.ru = " до ";
 
-        var InitComplete = false;  
-        
+        var InitComplete = false;
+
         var device = {};
-        device.enable = null;  
-        device.target = null; 
-        device.current = 0;  
-        device.state = "off"; 
+        device.enable = null;
+        device.target = null;
+        device.current = 0;
+        device.state = "off";
         device.hysteresis = config.hysteresis;
         device.relay = 0;
-        device.regultype = config.regultype;  
+        device.regultype = config.regultype;
         device.strategy = config.strategy;
 
         function SetAllMeta (){
@@ -67,21 +67,21 @@ module.exports = function(RED) {
             metatarget.max = config.max;
             metatarget.readonly = false;
             metatarget.type = "range";
-            metatarget.order = 4;           
+            metatarget.order = 4;
             metarelay.title = {"en":ParametrsNames[param].relay.en, "ru":ParametrsNames[param].relay.ru};
             metarelay.readonly = true;
             metarelay.type = "switch";
             metarelay.order = 5;
-            metacurrent.type = ParametrsNames[param].current.type;                    
+            metacurrent.type = ParametrsNames[param].current.type;
         }
-        
+
         server.on("online",()=>{
 			node.status({fill:"green",shape:"dot",text:"connect"});
 		});
 
-		server.on("offline",()=>{   
+		server.on("offline",()=>{
 			node.status({fill:"red",shape:"dot",text:"no connect"});
-		});  
+		});
 
 
         function waitForMessage(timeout) {
@@ -93,10 +93,10 @@ module.exports = function(RED) {
 
                 function onMessage(receivedTopic, message) {
                     const topicParts = receivedTopic.split('/');
-                    if (topicParts[2] == name && topicParts.length == 5 && !(count == 2)) {                          
+                    if (topicParts[2] == name && topicParts.length == 5 && !(count == 2)) {
                         if (topicParts[4] == "target") {device.target = message.toString(); count += 1; }
-                        if (topicParts[4] == "enable") {device.enable = message.toString(); count += 1; }    
-                        if (count == 2) {clearTimeout(timer);} 
+                        if (topicParts[4] == "enable") {device.enable = message.toString(); count += 1; }
+                        if (count == 2) {clearTimeout(timer);}
                     }
                 }
                 server.mqtt.on('message', onMessage);
@@ -110,7 +110,7 @@ module.exports = function(RED) {
             } catch (error) {
                 node.error(`Ошибка: ${error.message}`);
                 device.target = config.target || config.min;
-                device.enable = 0;                
+                device.enable = 0;
                 node.log('Стартовое собщение не получено в течение 500 мс');
             }
         }
@@ -121,7 +121,7 @@ module.exports = function(RED) {
             meta.name = config.title;
             meta.title = {};
             meta.title.ru = config.title;
-            meta.driver = driver; 
+            meta.driver = driver;
             server.publishToTopic(topic + "/meta/name", config.title, true);
             server.publishToTopic(topic + "/meta/driver", driver, true);
             server.publishToTopic(topic + "/meta", JSON.stringify(meta), true);
@@ -130,7 +130,7 @@ module.exports = function(RED) {
             server.publishToTopic(topic + "/controls/target/meta", JSON.stringify(metatarget), true);
             server.publishToTopic(topic + "/controls/target/on", device.target.toString(), true);
             server.publishToTopic(topic + "/controls/current/meta", JSON.stringify(metacurrent), true);
-            server.publishToTopic(topic + "/controls/state/meta", JSON.stringify(metastate), true);        
+            server.publishToTopic(topic + "/controls/state/meta", JSON.stringify(metastate), true);
             server.publishToTopic(topic + "/controls/relay/meta", JSON.stringify(metarelay), true);
             InitComplete = true;
         }
@@ -143,26 +143,25 @@ module.exports = function(RED) {
             if (device.strategy == "humidification" &&  device.relay == 1 ) statestr = device.state.toString() + upto[locale] + device.target.toString() + "%";
             if (device.strategy == "drying" &&  device.relay == 1 ) statestr = device.state.toString() + to[locale] + device.target.toString() + "%";
             if (device.enable == 0 ) statestr = device.state.toString();
-            node.status({fill:"green",shape:"dot", text:statestr}); 
-            if (config.invertrelay == true) device.relay = 1 - device.relay;
+            node.status({fill:"green",shape:"dot", text:statestr});
             server.publishToTopic(topic + "/controls/state", device.state.toString(), true);
             server.publishToTopic(topic + "/controls/target", device.target.toString(), true);
             server.publishToTopic(topic + "/controls/enable", device.enable.toString(), true);
-            server.publishToTopic(topic + "/controls/relay", device.relay.toString(), true); 
+            server.publishToTopic(topic + "/controls/relay", device.relay.toString(), true);
             server.publishToTopic(topic + "/controls/current", device.current.toString(), true);
-            node.send({ payload: device.relay.toString() });
-        }    
+            node.send({ payload: config.invertrelay ? 1 - relay.toString(): relay.toString()});
+        }
 
 
         this.on('input', (msg, send, done)=>{
             if (InitComplete == false) return;
-            if (typeof msg.payload != 'number'){  
+            if (typeof msg.payload != 'number'){
 			 	node.error("Неверный тип! msg.payload должен быть числом.");
 			 	if (done) {done();}
 			 	return;
 			};
 
-			if (msg.payload == device.current){ 
+			if (msg.payload == device.current){
 			 	node.debug("Значение не изменилось. Отмена обновления.");
 			 	if (done) {done();}
 			 	return;
@@ -170,16 +169,16 @@ module.exports = function(RED) {
     		device.current = msg.payload;
             RegulatorLogic ()
 			WriteValuesToMQTT ();
-			if (done) {done();} 
+			if (done) {done();}
 		});
 
         function RegulatorLogic () {
-            if (device.enable == 0) { device.state = ParametrsNames.Temperature.states.off[locale]; device.relay = 0; }         
+            if (device.enable == 0) { device.state = ParametrsNames.Temperature.states.off[locale]; device.relay = 0; }
             else switch (device.regultype) {
                 case "thermostat":
                     if (device.strategy == "heating"){
                         if ( Number(device.current) < Number(device.target) - Number(device.hysteresis)) {
-                            device.state = ParametrsNames.Temperature.states.head[locale]; device.relay = 1;                            
+                            device.state = ParametrsNames.Temperature.states.head[locale]; device.relay = 1;
                             break;
                         }
                         if ( Number(device.current) > Number(device.target) + Number(device.hysteresis)) {
@@ -202,7 +201,7 @@ module.exports = function(RED) {
                 case "hygrostat":
                     if (device.strategy == "humidification"){
                         if ( Number(device.current) < Number(device.target) - Number(device.hysteresis)) {
-                            device.state = ParametrsNames.Humidity.states.humidification[locale]; device.relay = 1;                            
+                            device.state = ParametrsNames.Humidity.states.humidification[locale]; device.relay = 1;
                             break;
                         }
                         if ( Number(device.current) > Number(device.target) + Number(device.hysteresis)) {
@@ -228,12 +227,12 @@ module.exports = function(RED) {
         server.mqtt.on('message', (topic, message) => {
             if (InitComplete == false) return;
             const topicParts = topic.split('/');
-            if (topicParts[2] == name && topicParts[5] == "on") { 
+            if (topicParts[2] == name && topicParts[5] == "on") {
                 if (topicParts[4] == "target") {device.target = message.toString(); RegulatorLogic (); WriteValuesToMQTT ();}
-                if (topicParts[4] == "enable") {device.enable = message.toString(); RegulatorLogic (); WriteValuesToMQTT ();}    
-            }            
+                if (topicParts[4] == "enable") {device.enable = message.toString(); RegulatorLogic (); WriteValuesToMQTT ();}
+            }
         });
-        
+
         server.mqtt.subscribe(basetopic + name +"/#", function (err) {
             if (err) {
                 node.error(`Ошибка подписки топик устройства ${basetopic + name +"/#"}: ${err.message}`);
@@ -241,16 +240,15 @@ module.exports = function(RED) {
                 node.log(`Успешно подписан на топик ${basetopic + name +"/#"}`);
                 server.topicpush(basetopic + name +"/#");
                 SetAllMeta ();
-                requestStartValue(); 
+                requestStartValue();
                 const checkInitComplete = setInterval(() => {
                     if (!(device.target == null) && !(device.enable == null)) {
                         clearInterval(checkInitComplete);
                         WriteInitialValuesToMQTT();
                     }
-                }, 100);    
+                }, 100);
             }
         });
     }
     RED.nodes.registerType("ST-Regulator", STRegulator);
 }
-
